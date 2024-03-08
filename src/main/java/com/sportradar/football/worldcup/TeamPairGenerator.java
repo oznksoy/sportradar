296cd39f6ae6e4d5f0f8b1cc0caef674e02d67f7
oznksoy <oznksoy@gmail.com>
2024-03-08 14:26:28 +0100

    [DELETE]: TeamPairGenerator is deleted

--- src/main/java/com/sportradar/football/worldcup/TeamPairGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.sportradar.football.worldcup;
-
-class TeamPairGenerator {
-
-    public TeamPair pair(String homeTeam, String awayTeam){
-        return new TeamPair(homeTeam, awayTeam);
-    }
-
-}
