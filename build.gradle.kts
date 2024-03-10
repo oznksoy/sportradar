632e1e72b7b5a78fc01cd7408fec15f9a036fed9
oznksoy <oznksoy@gmail.com>
2024-03-10 16:18:48 +0100

    [UPDATE]: Gradle bundle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.mockito:mockito-junit-jupiter:5.11.0"))
+    testImplementation("org.mockito:mockito-junit-jupiter")
 }
 
 tasks.test {
